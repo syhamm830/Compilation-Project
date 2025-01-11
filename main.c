a790024c228ab6f16453f56b2bca95632c5ac571
syhamm830 <153018200+syhamm830@users.noreply.github.com>
2025-01-11 10:32:09 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ int main(int argc, char *argv[]) {
     if (argc < 2) {
         printf("Usage: %s <source file>\n", argv[0]);
         return 1;
+        
     }
 
     FILE *source = fopen(argv[1], "r");
